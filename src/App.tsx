80faf6779a44ccb310af36954bdd1edae0b7e63f
JakkarinK <earth1277@gmail.com>
2023-03-26 16:10:59 +0700

    get api trending

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import { Navbar } from './components/navbar/Navbar';
-import { Home } from './components/Home';
-import { Recommended } from './components/Recommended';
-import { Trending } from './components/Trending';
-import { Compare } from './components/Compare';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { Navbar } from "./components/navbar/Navbar";
+import { Home } from "./components/Home";
+import { Recommended } from "./components/Recommended";
+import { Trending } from "./components/Trending";
+import { Compare } from "./components/Compare";
+
+import axios from "axios";
 
 function App() {
-
   return (
     <div className="App">
-      <Router basename='/'>
+      <Router basename="/">
         <Navbar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/' element={<Trending />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Trending />} />
         </Routes>
       </Router>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
